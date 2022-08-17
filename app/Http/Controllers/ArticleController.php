f686eb84a45088d7ef423b23426389195ee0f091
ayezinzinaung <AyeZin2Aung101120@gmail.com>
2022-08-17 17:18:56 +0630

    708 update

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -23,4 +23,41 @@ class ArticleController extends Controller
         'article' => $data
        ]);
     }
+    public function add()
+    {
+        $data = [
+            ["id" => 1, "name" => "News"],
+            ["id" => 2, "name" => "Tech"],
+        ];
+
+        return view ('articles.add', [
+            'categories' => $data
+        ]);
+    }
+    public function create()
+    {
+        $validator = validator(request()->all(), [
+            'title' => 'required',
+            'body' => 'required',
+            'category_id' => 'required',
+        ]);
+
+        if($validator->fails()) {
+            return back() ->withErrors($validator);
+        }
+        $article = new Article;
+        $article ->title = request() ->title;
+        $article ->body = request() ->body;
+        $article ->category_id = request() ->category_id;
+        $article ->save();
+
+        return redirect('/articles');
+    }
+    public function delete($id)
+    {
+        $article = Article::find($id);
+        $article->delete();
+
+        return redirect('/articles')->with('info', 'Article deleted');
+    }
 }
--- resources/views/articles/add.blade.php
+++ resources/views/articles/add.blade.php
@@ -1,7 +1,17 @@
-@extends('lauouts.app')
+@extends('layouts.app')
 
 @section('content')
  <div class="container">
+
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <ol>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ol>
+        </div>
+    @endif
     <form method="post">
         @csrf
         <div class="mb-3">
@@ -14,8 +24,15 @@
         </div>
         <div class="mb-3">
             <label>Category</label>
-            <select name="category_id" id=""></select>
+            <select class="form-select" name="category_id" id="">
+                @foreach ($categories as $category)
+                    <option value="{{ $category['id'] }}">
+                        {{ $category['name'] }}
+                    </option>
+                @endforeach
+            </select>
         </div>
+        <input type="submit" value="Add Article" class="btn btn-primary">
     </form>
  </div>
 @endsection
